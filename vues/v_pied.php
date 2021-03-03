cc1f7f5e515fc1d22d78d56be0e607f9267fc8a9
ansen <ansen@192.168.1.30>
2021-03-03 16:12:17 +0100

    1ere version

---
+++ vues/v_pied.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Vue Pied de page
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Anaelle Seneor
+ */
+?>
+        </div>
+    </body>
+</html>
+
+
